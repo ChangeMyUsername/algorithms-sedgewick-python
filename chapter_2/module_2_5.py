cf6dfa8947b2d24dce0e89a6e767712de7011664
Reginald.Wong <xyz123456789011@gmail.com>
2015-05-18 23:42:49 +0800

    kendall Tau algorithm implementation

--- chapter_2/module_2_5.py
+++ chapter_2/module_2_5.py
@@ -160,5 +160,47 @@ class California(object):
     def name(self, val):
         self._name = val
 
+
+# 2.5.19 practice, kendall tau algorithm implementation
+class KendallTau(object):
+    '''
+    >>> klt = KendallTau()
+    >>> klt.kendall_tau_count((0, 3, 1, 6, 2, 5, 4), (1, 0, 3, 6, 4, 2, 5))
+    4
+    '''
+    def kendall_tau_count(self, origin_list, count_list):
+        lst = [origin_list.index(count_list[i]) for i in range(len(count_list))]
+        aux = lst[:]
+        return self.count(lst, aux, 0, len(lst) - 1)
+
+    def count(self, lst, aux, low, high):
+        if low >= high:
+            return 0
+        mid = (low + high) / 2
+        lc = self.count(lst, aux, low, mid)
+        rc = self.count(lst, aux, mid + 1, high)
+        mc = self.merge_count(lst, aux, low, mid, high)
+        return lc + rc + mc
+
+    def merge_count(self, lst, aux, low, mid, high):
+        aux[low:high + 1] = lst[low:high + 1]
+        count, left, right = 0, low, mid + 1
+        for j in range(low, high + 1):
+            if left > mid:
+                lst[j] = aux[right]
+                right += 1
+            elif right > high:
+                lst[j] = aux[left]
+                left += 1
+            elif aux[left] < aux[right]:
+                lst[j] = aux[left]
+                left += 1
+            else:
+                lst[j] = aux[right]
+                right += 1
+                count += mid - left + 1
+        return count
+
+
 if __name__ == '__main__':
     doctest.testmod()

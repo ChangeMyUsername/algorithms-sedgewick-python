cff5ea9e0a1c42e75a30369665a8bc60c6ec116e
qwerty <xerxes@camxi.com>
2015-05-21 00:18:51 +0800

    add max_val, min_val, select, rank(not so sure) function and comment

--- chapter_3/module_3_2.py
+++ chapter_3/module_3_2.py
@@ -60,11 +60,20 @@ class BST(object):
         self._root = None
 
     def size(self):
+        '''
+        return the node's amount of the binary search tree.
+        '''
         if not self._root:
             return 0
         return self._root.size
 
     def get(self, key):
+        '''
+        return the corresponding value with the given key, iterate the whole tree,
+        if the current node's key is equal to the given key, then return the node's value.
+        if the current node's key is smaller than the given key, then jump to the right node of the current node,
+        else jump to the left node of the current node.
+        '''
         temp = self._root
         if not temp:
             return None
@@ -81,9 +90,14 @@ class BST(object):
         return None
 
     def put(self, key, val):
+        '''
+        insert a new node into the binary search tree, iterate the whole tree,
+        find the appropriate location for the new node and add the new node as the tree leaf.
+        '''
         temp = self._root
         inserted_node = None
         new_node = Node(key, val, 1)
+
         while temp:
             inserted_node = temp
             temp.size += 1
@@ -106,3 +120,64 @@ class BST(object):
                 inserted_node.left = new_node
 
             inserted_node.size = self.size(inserted_node.left) + self.size(inserted_node.right) + 1
+
+    def max_val(self):
+        '''
+        find the maximum value in the binary search tree.
+        '''
+        if not self._root:
+            return None
+        tmp = self._root
+        while tmp.right:
+            tmp = tmp.right
+        return tmp.val
+
+    def min_val(self):
+        '''
+        find the minimum value in the binary search tree.
+        '''
+        if not self._root:
+            return None
+        tmp = self._root
+        while tmp.left:
+            tmp = tmp.left
+        return tmp.val
+
+    def select(self, k):
+        '''
+        find the kth node of the binary search tree, the solution is similar with get() or put() function.
+        '''
+        assert isinstance(k, int) and k <= self.size()
+
+        if not self._root:
+            return None
+
+        tmp = self._root
+        while tmp:
+            tmp_size = tmp.left.size
+            if tmp_size > k:
+                tmp = tmp.left
+            elif tmp_size < k:
+                tmp = tmp.right
+                k = k - tmp_size - 1
+            else:
+                return tmp
+
+    def rank(self, key):
+        '''
+        find the rank of the node in the binary search tree by the given key.
+        '''
+        result = 0
+        if not self._root:
+            return -1
+        tmp = self._root
+
+        while tmp:
+            if tmp.key > key:
+                tmp = tmp.left
+            elif tmp.key < key:
+                result += tmp.left.size + 1
+                tmp = tmp.right
+            elif tmp.key == key:
+                result = tmp.left.size
+        return result

dc479eb0099458a32610d574b3a4b1008aaaba56
Reginald.Wong <xyz123456789011@gmail.com>
2015-07-06 23:15:05 +0800

    fix multiple bug in red-black tree based on the book's example java code, append some test cases for red black tree.

--- chapter_3/module_3_3.py
+++ chapter_3/module_3_3.py
@@ -13,6 +13,8 @@ class Node(object):
         self._value = value
         self._size = size
         self._color = color
+        self._left = None
+        self._right = None
 
     @property
     def color(self):
@@ -69,12 +71,56 @@ class Node(object):
 
 class RBTree(object):
 
+    '''
+    >>> rbt = RBTree()
+    >>> rbt.is_empty()
+    True
+    >>> rbt.size()
+    0
+    >>> for index, e in enumerate('EASYQUITION'):
+    ...     rbt.put(e, index)
+    ...
+    >>> rbt.check()
+    True
+    >>> node1 = rbt.get('A').value
+    >>> node1
+    1
+    >>> rbt.get('E').value
+    0
+    >>> rbt.get('Y').value
+    3
+    >>> rbt.get('N').value
+    10
+    >>> rbt.is_empty()
+    False
+    >>> rbt.size()
+    11
+    >>> rbt.min_val().value
+    1
+    >>> rbt.max_val().value
+    3
+    >>> rbt.delete_min()
+    >>> rbt.min_val().value
+    0
+    >>> rbt.delete_min()
+    >>> rbt.min_val().value
+    8
+    >>> rbt.delete_max()
+    >>> rbt.max_val().value
+    5
+    >>> rbt.delete_max()
+    >>> rbt.max_val().value
+    7
+    >>> rbt.check()
+    True
+    '''
+
     def __init__(self):
         self._root = None
 
     def __is_red(self, node):
         if not node:
-            return BLACK
+            return False
         return node.color == RED
 
     def size(self):
@@ -87,32 +133,40 @@ class RBTree(object):
         return 0 if not node else node.size
 
     def __rotate_left(self, node):
+        assert node and self.__is_red(node.right)
+
         rotate_node = node.right
         node.right = rotate_node.left
         rotate_node.left = node
         rotate_node.color = node.color
         node.color = RED
         rotate_node.size = node.size
-        node.size = self.__node_size(node.left) + self.__node_size(node.right)
+        node.size = self.__node_size(node.left) + self.__node_size(node.right) + 1
         return rotate_node
 
     def __rotate_right(self, node):
+        assert node and self.__is_red(node.left)
+
         rotate_node = node.left
         node.left = rotate_node.right
         rotate_node.right = node
         rotate_node.color = node.color
         node.color = RED
         rotate_node.size = node.size
-        node.size = self.__node_size(node.left) + self.__node_size(node.right)
+        node.size = self.__node_size(node.left) + self.__node_size(node.right) + 1
         return rotate_node
 
     def __flip_colors(self, node):
-        node.color = RED
-        node.left.color = BLACK
-        node.right.color = BLACK
+        assert node and node.left and node.right
+        assert (not self.__is_red(node) and self.__is_red(node.left) and self.__is_red(node.right) or
+         self.__is_red(node) and not self.__is_red(node.left) and not self.__is_red(node.right))
+
+        node.color = RED if node.color == BLACK else BLACK
+        node.left.color = RED if node.left.color == BLACK else BLACK
+        node.right.color = RED if node.right.color == BLACK else BLACK
 
     def get(self, key):
-        return self.__get(self._root)
+        return self.__get(self._root, key)
 
     def __get(self, node, key):
         tmp = node
@@ -122,8 +176,8 @@ class RBTree(object):
             elif tmp.key < key:
                 tmp = tmp.right
             else:
-                return tmp
-        return None
+                break
+        return tmp
 
     def min_val(self):
         return self.__min_val(self._root)
@@ -156,7 +210,7 @@ class RBTree(object):
         # a red-black tree could not exist two consecutive red left node,
         # in this case, rotate right, then the left node and right node is both red,
         # the next move would be flip both node's colors.
-        if self.__is_red(node.left) and self.__is_red(node.left.left):
+        if self.__is_red(node.left) and node.left.left and self.__is_red(node.left.left):
             node = self.__rotate_right(node)
 
         if self.__is_red(node.left) and self.__is_red(node.right):
@@ -166,6 +220,8 @@ class RBTree(object):
         return node
 
     def __balance(self, node):
+        assert node is not None
+
         if self.__is_red(node.right):
             node = self.__rotate_left(node)
 
@@ -179,7 +235,12 @@ class RBTree(object):
         return node
 
     def __move_red_left(self, node):
+        assert node is not None
+        assert self.__is_red(node) and not self.__is_red(node.left) and not self.__is_red(node.left.left)
+
         self.__flip_colors(node)
+        # if node.right.left node is red, that means there is one more node can be "borrow",
+        # then move one node to node's right side.
         if self.__is_red(node.right.left):
             node.right = self.__rotate_right(node.right)
             node = self.__rotate_left(node)
@@ -188,6 +249,10 @@ class RBTree(object):
     # 3.3.39 delete minimum key in red-black tree, the java implementation is on the book,
     # this is python implementation of the book's answer.
     def delete_min(self):
+
+        if self.is_empty():
+            return None
+
         # this is for keeping red-black tree's balance
         if not self.__is_red(self._root.left) and not self.__is_red(self._root.right):
             self._root.color = RED
@@ -198,6 +263,8 @@ class RBTree(object):
     def __delete_min(self, node):
         if not node.left:
             return None
+        # if node's left node and node's left's left node is not red, "borrow" one node
+        # from node's right side to keep the red-black tree balance.
         if not self.__is_red(node.left) and not self.__is_red(node.left.left):
             node = self.__move_red_left(node)
         node.left = self.__delete_min(node.left)
@@ -205,6 +272,8 @@ class RBTree(object):
 
     def __move_red_right(self, node):
         self.__flip_colors(node)
+        # this is the same priciple to the __move_red_left function, move one node from
+        # the node's right side if the two consecutive left node is not red.
         if not self.__is_red(node.left.left):
             node = self.__rotate_right(node)
         return node
@@ -238,7 +307,8 @@ class RBTree(object):
             self._root.color = BLACK
 
     def __delete(self, node, key):
-        if key  < node.key:
+        if key < node.key:
+            # same principle with delete_min function
             if not self.__is_red(node.left) and not self.__is_red(node.left.left):
                 node = self.__move_red_left(node)
             node.left = self.__delete(node.left, key)
@@ -259,6 +329,146 @@ class RBTree(object):
             else:
                 node.right = self.__delete(node.right, key)
         return self.__balance(node)
+
+    def select(self, k):
+        '''
+        find the kth node of the binary search tree,
+        the solution is similar with get() or put() function.
+        '''
+        assert isinstance(k, int) and k <= self.size()
+
+        if not self._root:
+            return None
+
+        tmp = self._root
+        while tmp:
+            tmp_size = self.__node_size(tmp.left)
+            if tmp_size > k:
+                tmp = tmp.left
+            elif tmp_size < k:
+                tmp = tmp.right
+                k = k - tmp_size - 1
+            else:
+                return tmp
+
+    def rank(self, key):
+        '''
+        find the rank of the node in the binary search tree by the given key.
+        '''
+        result = 0
+        if not self._root:
+            return -1
+        tmp = self._root
+
+        while tmp:
+            if tmp.key > key:
+                tmp = tmp.left
+            elif tmp.key < key:
+                result += self.__node_size(tmp.left) + 1
+                tmp = tmp.right
+            elif tmp.key == key:
+                result += self.__node_size(tmp.left)
+                break
+        return result
+
+    def max_val(self):
+        '''
+        find the maximum value in the binary search tree.
+        '''
+        if not self._root:
+            return None
+        tmp = self._root
+        while tmp.right:
+            tmp = tmp.right
+        return tmp
+
+    def min_val(self):
+        '''
+        find the minimum value in the binary search tree which start with specific node.
+        '''
+        if not self._root:
+            return None
+
+        tmp = self._root
+        while tmp.left:
+            tmp = tmp.left
+        return tmp
+
+    def keys(self):
+        return self.keys_range(self.min_val().key, self.max_val().key)
+
+    def keys_range(self, low, high):
+        queue = []
+        self.__keys(self._root, queue, low, high)
+        return queue
+
+    def __keys(self, node, queue, low, high):
+        if not node:
+            return
+        if low < node.key:
+            self.__keys(node.left, queue, low, high)
+        if low <= node.key and high >= node.key:
+            queue.append(node.key)
+        if high > node.key:
+            self.__keys(node.right, queue, low, high)
+
+    def is_rbt(self):
+        return self.__is_rbt(self._root)
+
+    def __is_rbt(self, node):
+        if not node:
+            return True
+        if self.__is_red(node.right):
+            return False
+        if node != self._root and self.__is_red(node) and self.__is_red(node.left):
+            return False
+        return self.__is_rbt(node.left) and self.__is_rbt(node.right)
+
+    def is_binary_tree(self):
+        return self.__is_binary_tree(self._root)
+
+    def __is_binary_tree(self, node):
+        if not node:
+            return True
+        if node.size != self.__node_size(node.left) + self.__node_size(node.right) + 1:
+            return False
+        return self.__is_binary_tree(node.left) and self.__is_binary_tree(node.right)
+
+    def is_ordered(self):
+        return self.__is_ordered(self._root, None, None)
+
+    def __is_ordered(self, node, min_key, max_key):
+        if not node:
+            return True
+        if min_key and node.key <= min_key:
+            return False
+        if max_key and node.key >= max_key:
+            return False
+        return (self.__is_ordered(node.left, min_key, node.key)
+                and self.__is_ordered(node.right, node.key, max_key))
+
+    def is_rank_consistent(self):
+        for i in range(self.size()):
+            if i != self.rank(self.select(i).key):
+                return False
+
+        for key in self.keys():
+            if key != self.select(self.rank(key)).key:
+                return False
+
+        return True
+
+    def check(self):
+        if not self.is_binary_tree():
+            return False
+        if not self.is_ordered():
+            return False
+        if not self.is_rank_consistent():
+            return False
+        if not self.is_rbt():
+            return False
+        return True
+
 
 if __name__ == '__main__':
     doctest.testmod()

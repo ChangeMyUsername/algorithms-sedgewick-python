6ba431e9d8cf4fadda279617f5592efd87e2dc33
Reginald.Wong <xyz123456789011@gmail.com>
2015-08-06 00:04:02 +0800

    add book's examples(Cycle.java and TwoColor.java) and not test yet; modify README.md

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # algorithms-sedgewick-python #
->    Algorthms by Robert Sedgewick 4th edition partial exercises in python, all the algorithms can be run with doctest, those non-programming exercises will be ignored, and some of other exercises are unsolved. If there are more python exercise solutions for this book, please make an issue or pull request. 
+>    Algorthms by Robert Sedgewick and Kevin Wayne 4th edition partial exercises in python, all the algorithms can be run with doctest. Those non-programming exercises will be excluded, and some of other exercises are still unsolved. If there are more python solutions for this book, please bring it up.
--- chapter_4/module_4_1.py
+++ chapter_4/module_4_1.py
@@ -48,6 +48,9 @@ class Graph(object):
     def get_adjacent_vertices(self, vertex):
         return self._adj.get(vertex, None)
 
+    def vertices(self):
+        return self._adj.keys()
+
     def degree(self, vertex):
         assert vertex in self._adj
         return self._adj[vertex].size()
@@ -154,6 +157,85 @@ class BreadthFirstPaths(object):
             tmp = self._edge_to[tmp]
         path.push(self._start)
         return path
+
+
+class ConnectedComponent(object):
+
+    """
+    """
+
+    def __init__(self, graph):
+        self._marked = defaultdict(bool)
+        self._id = defaultdict(int)
+        self._count = 0
+
+        for s in graph.vertices():
+            if not self._marked[s]:
+                self.dfs(graph, s)
+                self._count += 1
+
+    def dfs(self, graph, vertex):
+        self._marked[vertex] = True
+        self._id[vertex] = self._count
+        for s in graph.get_adjacent_vertices(vertex):
+            if not self._marked[vertex]:
+                self.dfs(graph, vertex)
+
+    def connected(self, vertex_1, vertex_2):
+        return self._id[vertex_1] == self._id[vertex_2]
+
+    def vertex_id(self, vertex):
+        return self._id[vertex]
+
+    def count(self):
+        return self._count
+
+
+class Cycle(object):
+
+    def __init__(self, graph):
+        self._marked = defaultdict(bool)
+        self._has_cycle = False
+        for vertex in graph.vertices():
+            if not self._marked[vertex]:
+                self.dfs(graph, vertex, vertex)
+
+    def dfs(self, graph, vertex_1, vertex_2):
+        self._marked[vertex_1] = True
+        for adj in graph.get_adjacent_vertices(vertex_1):
+            if not self._marked[adj]:
+                self.dfs(graph, adj, vertex_1)
+            else:
+                if adj != vertex_2:
+                    self.has_cycle = True
+
+    def has_cycle(self):
+        return self._has_cycle
+
+
+class TwoColor(object):
+
+    def __init__(self, graph):
+        self._marked = defaultdict(bool)
+        self._color = defaultdict(bool)
+        self._is_twocolorable = True
+
+        for vertex in graph.vertices():
+            if not self._marked[vertex]:
+                self.dfs(graph, vertex)
+
+    def dfs(self, graph, vertex):
+        self._marked[vertex] = True
+        for v in graph.get_adjacent_vertices(vertex):
+            if not self._marked[v]:
+                self._color[v] = self._color[vertex]
+                self.dfs(graph, v)
+            else:
+                if self._color[v] == self._color[vertex]:
+                    self._is_twocolorable = False
+
+    def is_bipartite(self):
+        return self._is_twocolorable
 
 if __name__ == '__main__':
     doctest.testmod()

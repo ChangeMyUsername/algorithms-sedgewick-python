52981ab5e2c9d33bb4683e554e57019117cbe278
Reginald.Wong <xyz123456789011@gmail.com>
2015-11-10 23:04:48 +0800

    add TernarySearchTree, switch some methods to iterative version.

--- chapter_5/module_5_2.py
+++ chapter_5/module_5_2.py
@@ -55,20 +55,32 @@ class Trie(object):
         return None
 
     def put(self, key, value):
-        self._root = self._put(self._root, key, value, 0)
+        # self._root = self._put(self._root, key, value, 0)
+        d = 0
+        tmp = self._root
+        while d <= len(key):
+            if not tmp:
+                tmp = Node()
+            if d == len(key):
+                tmp.val = value
+                break
+            char = key[d]
+            index = ord(char)
+            tmp = tmp.next_nodes[index]
+            d += 1
 
-    def _put(self, node, key, value, d):
-        if node is None:
-            node = Node()
+    # def _put(self, node, key, value, d):
+    #     if node is None:
+    #         node = Node()
 
-        if d == len(key):
-            node.val = value
-            return node
+    #     if d == len(key):
+    #         node.val = value
+    #         return node
 
-        char = key[d]
-        index = ord(char)
-        node.next_nodes[index] = self._put(node.next_nodes[index], key, value, d + 1)
-        return node
+    #     char = key[d]
+    #     index = ord(char)
+    #     node.next_nodes[index] = self._put(node.next_nodes[index], key, value, d + 1)
+    #     return node
 
     def keys(self):
         return self.keys_with_prefix('')
@@ -145,6 +157,94 @@ class Trie(object):
             if node.next_nodes[i]:
                 return node
         return None
+
+
+class TNode(object):
+
+    def __init__(self):
+        self._char = None
+        self._left = None
+        self._right = None
+        self._mid = None
+        self._val = None
+
+    @property
+    def char(self):
+        return self._char
+
+    @char.setter
+    def char(self, new_char):
+        return self._char
+
+    @property
+    def left(self):
+        return self._left
+
+    @left.setter
+    def left(self, node):
+        self._left = node
+
+    @property
+    def right(self):
+        return self._right
+
+    @right.setter
+    def right(self, node):
+        self._right = node
+
+    @property
+    def mid(self):
+        return self._mid
+
+    @mid.setter
+    def mid(self, node):
+        self._mid = node
+
+    @property
+    def val(self):
+        return self._val
+
+    @val.setter
+    def val(self, value):
+        self._val = value
+
+
+class TernarySearchTries(object):
+
+    def __init__(self):
+        self._root = None
+
+    def get(self, key):
+        tmp = self._root
+        d = 0
+        while tmp:
+            char = key[0]
+            if char < tmp.val:
+                tmp = tmp.left
+            elif char > tmp.val:
+                tmp = tmp.right
+            else:
+                if d < len(key) - 1:
+                    tmp = tmp.mid
+                    d += 1
+        return tmp.val if tmp else None
+
+    def put(self, key, value):
+        tmp = self._root
+        d = 0
+        while d < len(key):
+            char = key[d]
+            if not tmp:
+                tmp = TNode()
+                tmp.char = char
+            if char < tmp.char:
+                tmp = tmp.left
+            elif char > tmp.char:
+                tmp = tmp.right
+            else:
+                if d < len(key) - 1:
+                    tmp = tmp.mid
+                    d += 1
 
 if __name__ == '__main__':
     doctest.testmod()

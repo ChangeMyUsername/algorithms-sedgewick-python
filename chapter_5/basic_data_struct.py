0e4067a6250e3d5d1d3bb4e03770c95280856da6
Reginald.Wong <xyz123456789011@gmail.com>
2016-03-19 23:09:16 +0800

    add NFA for chapter 5.4, untested

--- chapter_5/basic_data_struct.py
+++ chapter_5/basic_data_struct.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- encoding:UTF-8 -*-
+import copy
+from collections import defaultdict
 
 
 class Node(object):
@@ -66,3 +68,124 @@ class Queue(object):
             self._size -= 1
             return val
         return None
+
+
+class Stack(object):
+
+    def __init__(self):
+        self._first = None
+        self._size = 0
+
+    def __iter__(self):
+        node = self._first
+        while node:
+            yield node.val
+            node = node.next_node
+
+    def is_empty(self):
+        return self._first is None
+
+    def size(self):
+        return self._size
+
+    def push(self, val):
+        node = Node(val)
+        old = self._first
+        self._first = node
+        self._first.next_node = old
+        self._size += 1
+
+    def pop(self):
+        if self._first:
+            old = self._first
+            self._first = self._first.next_node
+            self._size -= 1
+            return old.val
+        return None
+
+    def peek(self):
+        if self._first:
+            return self._first.val
+        return None
+
+
+class Bag(object):
+
+    def __init__(self):
+        self._first = None
+        self._size = 0
+
+    def __iter__(self):
+        node = self._first
+        while node is not None:
+            yield node.val
+            node = node.next_node
+
+    def add(self, val):
+        node = Node(val)
+        old = self._first
+        self._first = node
+        self._first.next_node = old
+        self._size += 1
+
+    def is_empty(self):
+        return self._first is None
+
+    def size(self):
+        return self._size
+
+
+class Digragh(object):
+
+    def __init__(self, steps):
+        self._edges_size = 0
+        self._adj = {i: None for i in range(steps)}
+        self._vertices = set()
+
+    def vertices_size(self):
+        return len(self._vertices)
+
+    def edges_size(self):
+        return self._edges_size
+
+    def add_edge(self, start, end):
+        if self.has_edge(start, end) or start == end:
+            return
+        self._vertices.add(start)
+        self._vertices.add(end)
+        if not self._adj[start]:
+            self._adj[start] = Bag()
+        self._adj[start].add(end)
+        self._edges_size += 1
+
+    def get_adjacent_vertices(self, vertex):
+        return self._adj[vertex] if self._adj[vertex] is not None else []
+
+    def vertices(self):
+        return self._vertices
+
+    def reverse(self):
+        reverse_graph = Digragh()
+        for vertex in self.vertices():
+            for adjacent_vertex in self.get_adjacent_vertices(vertex):
+                reverse_graph.add_edge(adjacent_vertex, vertex)
+        return reverse_graph
+
+    def has_edge(self, start, end):
+        if not self._adj[start]:
+            return False
+        edge = next((i for i in self._adj[start] if i == end), None)
+        return edge is not None
+
+    def __repr__(self):
+        s = str(len(self._vertices)) + ' vertices, ' + str(self._edges_size) + ' edges\n'
+        for k in self._adj:
+            try:
+                lst = ' '.join([vertex for vertex in self._adj[k]])
+            except TypeError:
+                if self._adj[k]:
+                    lst = ' '.join([str(vertex) for vertex in self._adj[k]])
+                else:
+                    lst = ''
+            s += '{}: {}\n'.format(k, lst)
+        return s
--- chapter_5/module_5_4.py
+++ chapter_5/module_5_4.py
@@ -1,6 +1,94 @@
 #!/usr/bin/env python
 # -*- encoding:UTF-8 -*-
 import doctest
+from basic_data_struct import Digragh, Stack, Bag
+from collections import defaultdict
+
+
+class DirectedDFS(object):
+
+    def __init__(self, graph, *sources):
+        self._marked = defaultdict(bool)
+        for vertex in sources:
+            if not self._marked[vertex]:
+                self.dfs(graph, vertex)
+
+    def dfs(self, graph, vertex):
+        self._marked[vertex] = True
+        for adjacent_vertex in graph.get_adjacent_vertices(vertex):
+            if not self._marked[adjacent_vertex]:
+                self.dfs(graph, adjacent_vertex)
+
+    def marked(self, vertex):
+        return self._marked[vertex]
+
+
+class NFA(object):
+
+    '''
+    >>> nfa = NFA('(A*B|AC)D')
+    >>> nfa.recognizes('AAAABD')
+    True
+    >>> nfa2 = NFA('(A*B|AC)D')
+    >>> nfa2.recognizes('AAAAC')
+    False
+    >>> nfa3 = NFA('(a|(bc)*d)*')
+    >>> nfa3.recognizes('abcbcd')
+    True
+    >>> nfa4 = NFA('(a|(bc)*d)*')
+    >>> nfa4.recognizes('abcbcbcdaaaabcbcdaaaddd')
+    True
+    '''
+
+    def __init__(self, regexp):
+        self._regexp = regexp
+        self._ops = Stack()
+        self._graph = Digragh()
+        self._reg_len = len(self._regexp)
+
+        for i in range(self._reg_len):
+            lp = i
+            if self._regexp[i] == '(' or self._regexp == '|':
+                self._ops.push(i)
+            elif self._regexp[i] == ')':
+                or_op = self._ops.pop()
+                if self._regexp[or_op] == '|':
+                    lp = self._ops.pop()
+                    self._graph.add_edge(lp, or_op + 1)
+                    self._graph.add_edge(or_op, i)
+                else:
+                    lp = or_op
+            if i < self._reg_len - 1 and self._regexp[i + 1] == '*':
+                self._graph.add_edge(lp, i + 1)
+                self._graph.add_edge(i + 1, lp)
+            if self._regexp[i] in ('(', '*', ')'):
+                self._graph.add_edge(i, i + 1)
+
+    def recognizes(self, txt):
+        pc = Bag()
+        dfs = DirectedDFS(self._graph, (0,))
+        for v in self._graph.vertices():
+            if dfs.marked(v):
+                pc.add(v)
+
+        length = len(txt)
+        for i in range(length):
+            match = Bag()
+            for v in pc:
+                if v < self._reg_len:
+                    if self._regexp[v] == txt[i] or self._regexp[v] == '.':
+                        match.add(v + 1)
+
+            pc = Bag()
+            dfs = DirectedDFS(self._graph, match)
+            for v in self._graph.vertices():
+                if dfs.marked(v):
+                    pc.add(v)
+
+        for v in pc:
+            if v == self._reg_len:
+                return True
+        return False
 
 
 if __name__ == '__main__':

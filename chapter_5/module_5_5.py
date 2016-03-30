9b44fa3cbe7ca3c0a4f037a8523722119392efce
Reginald.Wong <xyz123456789011@gmail.com>
2016-03-30 23:52:59 +0800

    add some code for chapter 5.5

--- chapter_5/module_5_5.py
+++ chapter_5/module_5_5.py
@@ -2,6 +2,7 @@
 # -*- encoding:UTF-8 -*-
 import doctest
 import sys
+from basic_data_struct import MinPQ
 
 
 class Node(object):
@@ -18,12 +19,58 @@ class Node(object):
     def __cmp__(self, other):
         return self._freq - other._freq
 
+    @property
+    def char(self):
+        return self._char
+
+    @property
+    def freq(self):
+        return self._freq
+
+    @property
+    def left(self):
+        return self._left
+
+    @property
+    def right(self):
+        return self._right
+
 
 class Huffman(object):
 
     @staticmethod
     def compress():
-        pass
+        input_string = ''.join(sys.stdin.readlines())
+        frequency = [0] * 256
+        for i in input_string:
+            frequency[ord(i)] += 1
+
+        root = Huffman.build_trie(frequency)
+
+        st = [None] * 256
+        Huffman.build_code(st, root, '')
+
+    @staticmethod
+    def build_trie(freq):
+        min_pq = MinPQ()
+        for i in range(256):
+            if freq[i]:
+                min_pq.insert(chr(i), freq, None, None)
+
+        while min_pq.size() > 1:
+            left = min_pq.del_min()
+            right = min_pq.del_min()
+            parent = Node('\0', left.freq + right.freq, left, right)
+            min_pq.insert(parent)
+        return min_pq.del_min()
+
+    @staticmethod
+    def build_code(st, node, code):
+        if not node.is_leaf():
+            Huffman.build_code(st, node.left, code + '0')
+            Huffman.build_code(st, node.right, code + '1')
+        else:
+            st[ord(node.char)] = code
 
     @staticmethod
     def expand():

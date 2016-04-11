63b63debb01ddc02919ba190aab70b80b4e487a0
Reginald.Wong <xyz123456789011@gmail.com>
2016-04-11 22:19:25 +0800

    finish BTree(untested) for chapter 6.1, this version is completely different from the book's version

--- chapter_6/module_6_1.py
+++ chapter_6/module_6_1.py
@@ -29,6 +29,22 @@ class Node(object):
         self._m_size = k
         self._children = [None] * M_SIZE
 
+    @property
+    def children(self):
+        return self._children
+
+    @children.setter
+    def children(self, new_children):
+        self._children = new_children
+
+    @property
+    def m_size(self):
+        return self._m_size
+
+    @m_size.setter
+    def m_size(self, size):
+        self._m_size = size
+
 
 class BTree(object):
 
@@ -44,19 +60,41 @@ class BTree(object):
         return self._height
 
     def put(self, key, value):
-        return self._insert(self._root, key, value, self._height)
+        u = self._insert(self._root, key, value, self._height)
+        self._size += 1
+        tmp = Node(2)
+        tmp.children[0] = Entry(self._root.children[0].key, None, self._root)
+        tmp.children[1] = Entry(u.children[0].key, None, u)
+        self._root = tmp
+        self._height += 1
 
     def _insert(self, node, key, value, height):
         pos = 0
         new_entry = Entry(key, value, None)
         # external node
         if height == 0:
-            while pos < node._m_size:
+            while pos < node.m_size:
                 if node[pos] and key < node[pos].key:
                     break
                 pos += 1
         else:
-            pass
+            while pos < node.m_size:
+                if pos + 1 == node.m_size or key < node.children[pos + 1].key:
+                    u = self._insert(node.children[pos + 1], key, value, height - 1)
+                    if not u:
+                        return None
+                    new_entry.key = u.children[0].key
+                    new_entry.next_node = u
+                    break
+                pos += 1
+
+        for i in range(node.m_size, pos, -1):
+            node.children[i] = node.children[i - 1]
+        node.children[pos] = new_entry
+        node.m_size += 1
+        if node.m_size < M_SIZE:
+            return None
+        return self._split(node)
 
     def _split(self, node):
         split_node = Node(M_SIZE / 2)
@@ -69,4 +107,12 @@ class BTree(object):
         return self._search(self._root, key, self._height)
 
     def _search(self, node, key, height):
-        pass
+        if height == 0:
+            for i in range(node.m_size):
+                if node.children[i].key == key:
+                    return node.children[i].value
+        else:
+            for i in range(node.m_size):
+                if i + 1 == node.m_size or key < node.children[i + 1].key:
+                    return self._search(node.children[i].next, key, height - 1)
+        return None
